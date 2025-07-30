767af4825158b583d51ca94f2c67ecf3e3cdfddf
Sam Friedman <sam@golioth.io>
2025-07-30 17:39:53 -0400

    block: use memslab for block allocation
    Use a memory slab instead of malloc for allocating blocks. This
    ensures we do not exhaust memory with an unbounded number of
    allocations and allows us to stall the downlink block download.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- gateway/src/block.h
+++ gateway/src/block.h
@@ -6,9 +6,11 @@
 
 #include <stdbool.h>
 
+#include <zephyr/kernel.h>
+
 struct block;
 
-struct block *block_alloc(void *user_data, size_t size);
+struct block *block_alloc(void *user_data, size_t size, k_timeout_t timeout);
 void block_free(struct block *block);
 size_t block_length(const struct block *block);
 void block_mark_last(struct block *block);
