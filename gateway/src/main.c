golioth/bluetooth-gateway

commit 0f5a27376ecf0ed853cf104e1b005071a9ad7930

Add blockwise upload
Moves to a queue pushing blockwise uploads for uplink pouches, wrapping
logic around queuing and processing in a new uplink.c file replacing fifo.c.

Signed-off-by: Trond Snekvik <trond@golioth.io>

gateway/src/main.c
@@ -10,7 +10,7 @@
 #include <samples/common/net_connect.h>
 
 #include "bt.h"
-#include "fifo.h"
+#include "uplink.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
@@ -95,46 +95,16 @@ static inline void connect_to_cloud(void) {}
 
 int main(void)
 {
-    struct pouch_fifo_item *item;
     int err;
 
     connect_to_cloud();
+
+    pouch_uplink_init(client);
 
     err = bt_app_start();
     if (err)
     {
         return err;
-    }
-
-    while (true)
-    {
-        item = k_fifo_get(&pouches_fifo, K_FOREVER);
-        if (!item)
-        {
-            LOG_ERR("Failed to get FIFO msg");
-            continue;
-        }
-
-        LOG_INF("Sending pouch to cloud");
-        LOG_HEXDUMP_DBG(item->data, item->len, "pouch");
-
-        if (!IS_ENABLED(CONFIG_GATEWAY_CLOUD))
-        {
-            goto free_item;
-        }
-
-        enum golioth_status status =
-            golioth_gateway_uplink_sync(client, item->data, item->len, SYNC_TIMEOUT_S);
-        if (status != GOLIOTH_OK)
-        {
-            LOG_ERR("Failed to deliver pouch: %d", status);
-            goto free_item;
-        }
-
-        LOG_INF("Successfully sent");
-
-    free_item:
-        free(item);
     }
 
     return 0;

gateway/CMakeLists.txt
@@ -6,6 +6,6 @@ project(gateway)
 
 target_sources(app PRIVATE
   src/bt.c
-  src/fifo.c
+  src/uplink.c
   src/main.c
 )

gateway/Kconfig
@@ -10,4 +10,17 @@ config GATEWAY_CLOUD
 	bool "Send pouches to cloud"
 	default y
 
+if GATEWAY_CLOUD
+
+config GATEWAY_UPLINK_TIMEOUT
+    int "Uplink timeout"
+    default 10
+    help
+      Timeout for uplink in seconds. This is the maximum time the
+      gateway will wait for an uplink to be sent to the cloud. If the
+      timeout is reached, the gateway will stop waiting and continue
+      with other tasks.
+
+endif # GATEWAY_CLOUD
+
 source "Kconfig.zephyr"

gateway/prj.conf
@@ -20,6 +20,7 @@ CONFIG_GOLIOTH_SAMPLE_SETTINGS_SHELL=y
 
 # Golioth Firmware SDK with all dependencies
 CONFIG_GOLIOTH_FIRMWARE_SDK=y
+CONFIG_GATEWAY_CLOUD=y
 
 # Application
 CONFIG_MAIN_STACK_SIZE=2048

gateway/src/bt.c
@@ -17,7 +17,7 @@
 #include <pouch/transport/ble_gatt/common/packetizer.h>
 
 #include "bt.h"
-#include "fifo.h"
+#include "uplink.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(bt);
@@ -56,11 +56,7 @@ struct golioth_node_info
         struct bt_gatt_discover_params discover_params;
         struct bt_gatt_read_params read_params;
     };
-    struct
-    {
-        struct net_buf_simple buf;
-        uint8_t buf_data[256];
-    } scratch;
+    struct pouch_uplink *uplink;
 };
 
 static struct golioth_node_info connected_nodes[CONFIG_BT_MAX_CONN];
@@ -159,16 +155,18 @@ static void start_scan(void)
 
 /* Callback for handling BLE GATT Uplink Response */
 static uint8_t tf_uplink_read_cb(struct bt_conn *conn,
-                                 uint8_t err,
+                                 uint8_t read_err,
                                  struct bt_gatt_read_params *params,
                                  const void *data,
                                  uint16_t length)
 {
-    if (err)
+    if (read_err)
     {
-        LOG_ERR("Failed to read BLE GATT Uplink (err %d)", err);
+        LOG_ERR("Failed to read BLE GATT Uplink (err %d)", read_err);
         return BT_GATT_ITER_STOP;
     }
+
+    struct golioth_node_info *node = &connected_nodes[bt_conn_index(conn)];
 
     bool is_first = false;
     bool is_last = false;
@@ -187,30 +185,17 @@ static uint8_t tf_uplink_read_cb(struct bt_conn *conn,
         LOG_HEXDUMP_INF(data, length, "[READ] BLE GATT Uplink");
     }
 
-    struct net_buf_simple *buf = &connected_nodes[bt_conn_index(conn)].scratch.buf;
-
-    net_buf_simple_add_mem(buf, payload, payload_len);
+    int err = pouch_uplink_write(node->uplink, payload, payload_len, is_last);
+    if (err)
+    {
+        LOG_ERR("Failed to write to pouch (err %d)", err);
+        bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+        return BT_GATT_ITER_STOP;
+    }
 
     if (is_last)
     {
         bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
-
-        LOG_HEXDUMP_DBG(buf->data, buf->len, "pouch");
-
-        struct pouch_fifo_item *item;
-
-        item = malloc(sizeof(*item) + buf->len);
-        if (!item)
-        {
-            LOG_ERR("Failed to alloc new item");
-            return BT_GATT_ITER_STOP;
-        }
-
-        memcpy(item->data, buf->data, buf->len);
-        item->len = buf->len;
-
-        k_fifo_put(&pouches_fifo, item);
-
         return BT_GATT_ITER_STOP;
     }
 
@@ -320,13 +305,19 @@ static void bt_connected(struct bt_conn *conn, uint8_t err)
 
     LOG_INF("Connected: %s", addr);
 
-    uint8_t conn_idx = bt_conn_index(conn);
-    memset(&connected_nodes[conn_idx], 0, sizeof(connected_nodes[conn_idx]));
+    struct golioth_node_info *node = &connected_nodes[bt_conn_index(conn)];
 
-    struct net_buf_simple *buf = &connected_nodes[conn_idx].scratch.buf;
-    net_buf_simple_init(buf, 0);
+    node->uplink = pouch_uplink_open();
+    if (node->uplink == NULL)
+    {
+        LOG_ERR("Failed to open pouch uplink");
+        bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+        return;
+    }
 
-    struct bt_gatt_discover_params *discover_params = &connected_nodes[conn_idx].discover_params;
+    memset(&node->attr_handles, 0, sizeof(node->attr_handles));
+
+    struct bt_gatt_discover_params *discover_params = &node->discover_params;
 
     discover_params->func = discover_func;
     discover_params->type = BT_GATT_DISCOVER_PRIMARY;
@@ -346,6 +337,13 @@ static void bt_disconnected(struct bt_conn *conn, uint8_t reason)
     char addr[BT_ADDR_LE_STR_LEN];
 
     bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+    struct golioth_node_info *node = &connected_nodes[bt_conn_index(conn)];
+    if (node->uplink)
+    {
+        pouch_uplink_close(node->uplink);
+        node->uplink = NULL;
+    }
 
     LOG_INF("Disconnected: %s, reason 0x%02x %s", addr, reason, bt_hci_err_to_str(reason));
 

gateway/src/fifo.c
@@ -1,9 +1,0 @@
-/*
- * Copyright (c) 2025 Golioth, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include "fifo.h"
-
-K_FIFO_DEFINE(pouches_fifo);

gateway/src/fifo.h
@@ -1,21 +1,0 @@
-/*
- * Copyright (c) 2025 Golioth, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#ifndef __FIFO_H__
-#define __FIFO_H__
-
-#include <zephyr/kernel.h>
-
-extern struct k_fifo pouches_fifo;
-
-struct pouch_fifo_item
-{
-    sys_sfnode_t node;
-    size_t len;
-    uint8_t data[];
-};
-
-#endif /* __FIFO_H__ */

gateway/src/uplink.c
@@ -1,0 +1,243 @@
+/*
+ * Copyright (c) 2025 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+
+#include <zephyr/sys/atomic_types.h>
+
+#include <golioth/gateway.h>
+#include <golioth/stream.h>
+
+#include "uplink.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(uplink);
+
+enum pouch_flags
+{
+    POUCH_UPLINK_CLOSED,
+    POUCH_UPLINK_SENDING,
+};
+
+struct pouch_block
+{
+    sys_snode_t node;
+    size_t len;
+    uint8_t data[CONFIG_GOLIOTH_BLOCKWISE_UPLOAD_MAX_BLOCK_SIZE];
+};
+
+struct pouch_uplink
+{
+    struct blockwise_transfer *session;
+    uint32_t block_idx;
+    atomic_t flags;
+    struct pouch_block *wblock;
+    struct pouch_block *rblock;
+    sys_slist_t queue;
+};
+
+static struct golioth_client *client;
+
+static void process_uplink(struct pouch_uplink *uplink);
+
+static void cleanup_uplink(struct pouch_uplink *uplink)
+{
+    golioth_gateway_uplink_finish(uplink->session);
+
+    sys_snode_t *n;
+    while ((n = sys_slist_get(&uplink->queue)) != NULL)
+    {
+        free(CONTAINER_OF(n, struct pouch_block, node));
+    }
+
+    free(uplink->wblock);
+    free(uplink->rblock);
+    free(uplink);
+}
+
+static void block_upload_callback(struct golioth_client *client,
+                                  enum golioth_status status,
+                                  const struct golioth_coap_rsp_code *coap_rsp_code,
+                                  const char *path,
+                                  size_t block_size,
+                                  void *arg)
+{
+    struct pouch_uplink *uplink = arg;
+
+    if (!atomic_test_and_clear_bit(&uplink->flags, POUCH_UPLINK_SENDING))
+    {
+        LOG_ERR("Not sending");
+        return;
+    }
+
+    free(uplink->rblock);
+    uplink->rblock = NULL;
+
+    if (status != GOLIOTH_OK)
+    {
+        LOG_ERR("Failed to deliver block: %d", status);
+        cleanup_uplink(uplink);
+        return;
+    }
+
+    process_uplink(uplink);
+}
+
+static void process_uplink(struct pouch_uplink *uplink)
+{
+    enum golioth_status status;
+    if (atomic_test_and_set_bit(&uplink->flags, POUCH_UPLINK_SENDING))
+    {
+        LOG_DBG("Already processing queue");
+        return;
+    }
+
+    bool closed = atomic_test_bit(&uplink->flags, POUCH_UPLINK_CLOSED);
+
+    sys_snode_t *n = sys_slist_get(&uplink->queue);
+    if (n == NULL)
+    {
+        LOG_DBG("No blocks to process");
+        if (closed)
+        {
+            cleanup_uplink(uplink);
+            return;
+        }
+
+        atomic_clear_bit(&uplink->flags, POUCH_UPLINK_SENDING);
+        return;
+    }
+
+    uplink->rblock = CONTAINER_OF(n, struct pouch_block, node);
+
+    LOG_DBG("Processing block %zu of size %zu", uplink->block_idx, uplink->rblock->len);
+
+    status = golioth_gateway_uplink_block(uplink->session,
+                                          uplink->block_idx++,
+                                          uplink->rblock->data,
+                                          uplink->rblock->len,
+                                          sys_slist_is_empty(&uplink->queue) && closed,
+                                          block_upload_callback,
+                                          uplink);
+    if (status != GOLIOTH_OK)
+    {
+        LOG_ERR("Failed to deliver block: %d", status);
+        cleanup_uplink(uplink);
+    }
+}
+
+static struct pouch_block *block_alloc(struct pouch_uplink *uplink)
+{
+    struct pouch_block *block = malloc(sizeof(struct pouch_block));
+    if (block == NULL)
+    {
+        LOG_ERR("Failed to alloc block");
+        return NULL;
+    }
+
+    block->len = 0;
+
+    return block;
+}
+
+static void submit_block(struct pouch_uplink *uplink)
+{
+    LOG_DBG("Submitting block of size %zu", uplink->wblock->len);
+    sys_slist_append(&uplink->queue, &uplink->wblock->node);
+    uplink->wblock = NULL;
+}
+
+int pouch_uplink_write(struct pouch_uplink *uplink,
+                       const uint8_t *payload,
+                       size_t len,
+                       bool is_last)
+{
+    while (len)
+    {
+        if (uplink->wblock != NULL && uplink->wblock->len == sizeof(uplink->wblock->data))
+        {
+            submit_block(uplink);
+        }
+
+        if (uplink->wblock == NULL)
+        {
+            uplink->wblock = block_alloc(uplink);
+            if (uplink->wblock == NULL)
+            {
+                LOG_ERR("Failed to alloc new block");
+                return -ENOMEM;
+            }
+        }
+
+        size_t bytes_to_copy = MIN(len, sizeof(uplink->wblock->data) - uplink->wblock->len);
+
+        memcpy(&uplink->wblock->data[uplink->wblock->len], payload, bytes_to_copy);
+        uplink->wblock->len += bytes_to_copy;
+
+        len -= bytes_to_copy;
+        payload += bytes_to_copy;
+    }
+
+    if (is_last)
+    {
+        pouch_uplink_close(uplink);
+    }
+    else
+    {
+        process_uplink(uplink);
+    }
+
+    return 0;
+}
+
+void pouch_uplink_init(struct golioth_client *c)
+{
+    client = c;
+}
+
+struct pouch_uplink *pouch_uplink_open(void)
+{
+    struct pouch_uplink *uplink = malloc(sizeof(struct pouch_uplink));
+    if (uplink == NULL)
+    {
+        return NULL;
+    }
+
+    uplink->wblock = block_alloc(uplink);
+    if (uplink->wblock == NULL)
+    {
+        free(uplink);
+        return NULL;
+    }
+
+    uplink->session = golioth_gateway_uplink_start(client);
+    if (uplink->session == NULL)
+    {
+        LOG_ERR("Failed to start blockwise upload");
+        free(uplink->wblock);
+        free(uplink);
+        return NULL;
+    }
+
+    uplink->rblock = NULL;
+    uplink->block_idx = 0;
+    atomic_set(&uplink->flags, 0);
+    sys_slist_init(&uplink->queue);
+
+    return uplink;
+}
+
+void pouch_uplink_close(struct pouch_uplink *uplink)
+{
+    atomic_set_bit(&uplink->flags, POUCH_UPLINK_CLOSED);
+
+    if (uplink->wblock != NULL)
+    {
+        submit_block(uplink);
+    }
+
+    process_uplink(uplink);
+}

commit 681861df0c6d1fc124b51b8aff85667b103739b7

gateway: drop unused SYNC_TIMEOUT_S
Fixes: 0f5a27376ecf ("Add blockwise upload")

Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

gateway/src/main.c
@@ -25,8 +25,6 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-#define SYNC_TIMEOUT_S 2
-
 struct golioth_client *client;
 
 #ifdef CONFIG_GATEWAY_CLOUD