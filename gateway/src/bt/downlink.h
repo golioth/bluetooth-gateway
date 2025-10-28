c8f96476f8b9275f93ae5093ad745c63d2133d90
Marcin Niestroj <m.niestroj@emb.dev>
2025-10-28 14:37:35 +0100

    gateway: add '#pragma once' to header files
    Some heaer files were missing it.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- gateway/src/bt/downlink.h
+++ gateway/src/bt/downlink.h
@@ -4,5 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#pragma once
+
 struct downlink_context *gateway_downlink_start(struct bt_conn *conn);
 void gateway_downlink_cleanup(struct bt_conn *conn);
